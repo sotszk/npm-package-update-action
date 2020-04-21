1bdf88e9b2c967605dd2181ea5735fc38c70518d
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-21 22:26:20 +0900

    :wrench: apply eslint with prettier

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 const core = require('@actions/core');
 const wait = require('./wait');
 
-
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
+  try {
     const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+    console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
     await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
 
     core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
