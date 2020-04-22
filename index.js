234d683f22a6b67875e269007b8a8fb53d1a3632
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-22 22:20:31 +0900

    update

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+// const github = require('@actions/github');
+// const wait = require('./wait');
 
-// most @actions toolkit packages have async methods
 async function run() {
   try {
-    const input = core.getInput('input');
-    core.debug(input);
-    await wait(2000);
-    core.setOutput('out', input);
+    const executeDirs = core.getInput('execute_directories');
+    console.log(executeDirs);
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
+module.exports = run;
--- index.test.js
+++ index.test.js
@@ -3,6 +3,13 @@ const process = require('process');
 const cp = require('child_process');
 const path = require('path');
 
+// const run = require('.');
+const nock = require('nock');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+
+nock.disableNetConnect();
+
 test('throws invalid number', async () => {
   await expect(wait('foo')).rejects.toThrow('milleseconds not a number');
 });
@@ -21,3 +28,43 @@ test('test runs', () => {
   const ip = path.join(__dirname, 'index.js');
   console.log(cp.execSync(`node ${ip}`).toString());
 });
+
+// beforeEach(() => {
+//   jest.resetModules();
+
+//   github.context.payload = {
+//     action: 'opened',
+//     pull_request: {
+//       number: 1,
+//     },
+//   };
+// });
+
+// describe('run action', () => {
+//   test('comments on PR', async (done) => {
+//     process.env['INPUT_MESSAGE'] = 'Test Comment';
+//     process.env['GITHUB_REPOSITORY'] = 'testorg/testrepo';
+//     process.env['GITHUB_TOKEN'] = 'test-github-token';
+
+//     nock('https://api.github.com')
+//       .post(
+//         '/repos/testorg/testrepo/issues/1/comments',
+//         ({ body }) => body === 'Test Comment'
+//       )
+//       .reply(200, {
+//         html_url: 'https://github.com/testorg/testrepo/issues/1#issuecomment-1',
+//       });
+//     const setOutputMock = jest.spyOn(core, 'setOutput');
+
+//     await run();
+
+//     expect(10).toBe(10);
+
+//     expect(setOutputMock).toHaveBeenCalledWith(
+//       'commentUrl',
+//       'https://github.com/testorg/testrepo/issues/1#issuecomment-1'
+//     );
+
+//     done();
+//   });
+// });
