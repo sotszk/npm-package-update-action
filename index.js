dc19a9b4c1cd2156293ef451fae8ceb16ef9a521
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-23 14:28:02 +0900

    wip

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const core = require('@actions/core');
+const io = require('@actions/io');
+const os = require('os');
 // const github = require('@actions/github');
-// const wait = require('./wait');
+
+async function getOptions() {
+  const executeDirectories = core
+    .getInput('execute_directories')
+    .split(os.EOL)
+    .map((item) => item.trim());
+
+  return {
+    executeDirectories:
+      executeDirectories.length === 1 && executeDirectories[0].length === 0
+        ? null
+        : executeDirectories,
+  };
+}
+
+async function executeOutdated() {
+  return [];
+}
 
 async function run() {
   try {
-    const executeDirs = core.getInput('execute_directories');
-    console.log(executeDirs.length);
+    core.debug('Inside try block');
+
+    const result = [];
+
+    const which = await io.which('npm', true);
+    console.log('which', which);
+
+    const options = await getOptions();
+    console.log(options);
+
+    if (options.executeDirectories === null) {
+      const packages = executeOutdated();
+      packages.forEach((item) => result.push(item));
+    }
+
+    core.setOutput('has_npm_update', true);
+    core.setOutput('has_major_npm_update', false);
+    core.setOutput('npm_update_formatted', '');
+    core.setOutput('npm_update_json', JSON.stringify(result));
   } catch (error) {
     core.setFailed(error.message);
   }
--- index.test.js
+++ index.test.js
@@ -1,35 +1,12 @@
-const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
-
 const run = require('.');
-// const nock = require('nock');
-// const core = require('@actions/core');
-// const github = require('@actions/github');
 
-test('throws invalid number', async () => {
-  await expect(wait('foo')).rejects.toThrow('milleseconds not a number');
-});
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThan(450);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 500;
-  const ip = path.join(__dirname, 'index.js');
-  console.log(cp.execSync(`node ${ip}`).toString());
-});
-
-test('just run', async (done) => {
-  process.env.INPUT_EXECUTE_DIRECTORIES = ['fuga', 'hoge'];
-  await run();
-  console.log(cp.execSync('node -v').toString());
-  done();
+describe('run', () => {
+  test('run', async (done) => {
+    process.env.INPUT_EXECUTE_DIRECTORIES = `
+      aa
+      bb
+    `;
+    await run();
+    done();
+  });
 });
