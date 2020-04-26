d7a38a286311d988dc3b6ce45b61803cb3e97a4d
SotaSuzuki <3.3sota.suzuki@gmail.com>
2020-04-26 10:33:12 +0900

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const io = require('@actions/io');
+const { exec } = require('@actions/exec');
 const os = require('os');
 // const github = require('@actions/github');
 
@@ -17,8 +18,39 @@ async function getOptions() {
   };
 }
 
-async function executeOutdated() {
-  return [];
+async function executeOutdated(executeDirectory) {
+  const execOptions = {};
+  if (executeDirectory) {
+    execOptions.cwd = executeDirectory;
+  }
+
+  let stdout = '';
+  execOptions.listeners = {
+    stdout: (data) => {
+      stdout += data.toString();
+    },
+  };
+  const args = ['--long', '--json'];
+
+  await exec('npm outdated', args, execOptions);
+
+  // ---
+  if (stdout.trim().length === 0) {
+    return [];
+  }
+
+  const json = JSON.parse(stdout);
+
+  return Object.keys(json).map((key) => {
+    const { current, wanted, latest, homepage } = json[key];
+    return {
+      name: key,
+      current,
+      wanted,
+      latest,
+      homepage,
+    };
+  });
 }
 
 async function run() {
@@ -34,16 +66,21 @@ async function run() {
     // console.log(options);
 
     if (options.executeDirectories === null) {
-      const packages = executeOutdated();
-      packages.forEach((item) => result.push(item));
+      const packages = await executeOutdated();
+      packages.forEach((pkg) => result.push(pkg));
+    } else {
+      for (let executeDirectory in options.executeDirectories) {
+        const packages = await executeOutdated(executeDirectory);
+        packages.forEach((pkg) => result.push(pkg));
+      }
     }
 
     core.setOutput('has_npm_update', 'true');
     core.setOutput('has_major_npm_update', 'false');
-    core.setOutput(
-      'npm_update_formatted',
-      options.executeDirectories.join(',')
-    ); // TODO
+    // core.setOutput(
+    //   'npm_update_formatted',
+    //   options.executeDirectories.join(',')
+    // );
     core.setOutput('npm_update_json', JSON.stringify(result));
   } catch (error) {
     core.setFailed(error.message);
